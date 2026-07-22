47d6d46a481d8578cc7c91245fe416e02c40780d
Titanius69 <149973614+Titanius69@users.noreply.github.com>
2026-07-22 13:06:58 +0200

    We got it ready for release

--- src/main/java/com/luminex_studios/luminstatus/LuminStatusPlugin.java
+++ src/main/java/com/luminex_studios/luminstatus/LuminStatusPlugin.java
@@ -46,7 +46,7 @@ import java.util.concurrent.TimeUnit;
 )
 public final class LuminStatusPlugin {
 
-    public static final String VERSION = "1.0.0";
+    public static final String VERSION = "v1.0.0";
 
     private final ProxyServer proxy;
     private final Logger logger;
